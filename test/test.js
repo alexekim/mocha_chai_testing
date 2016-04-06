ccf2c202fb6ae18abe120c73137873322d0bc6e8
Alex Kim <alexander.e.kim@gmail.com>
2016-04-06 09:42:33 -0500

    adding travis-ci test integration support

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var chai = require('chai') // requires the chai bdd library
+var expect = chai.expect; // require the expect chai library
+var db = db || {};        // mock database object
+// ^^^ this is 'namespacing'
+
+// describe a set of tests
+describe('How BDD testing should work for newbies', function(){
+  // our callbacks contain tests
+  // CCT (callbacks contain tests)
+  // IT is a test
+  it('should have a passing grade in mocha', function(){
+    var grade = 8; // 8/10 to pass
+    expect(grade).to.be.above(7);
+  });
+
+});
